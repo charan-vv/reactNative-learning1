e5e803407645b67444ab4bb40e39f5df6501b047
charan-vv <venkatacharan@coderzvisiontech.com>
2025-03-12 19:34:27 +0530

    Added the name and changes done

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,52 +1,3 @@
-// import React, {useEffect, useState} from 'react';
-// import {Login, ForgotPassword, SignUp, TextToSpeech} from '../Screens';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
-// import SplashScreen from 'react-native-splash-screen';
-// import {Platform, View, ActivityIndicator} from 'react-native';
-
-// const Stack = createStackNavigator();
-// const options = {headerShown: false};
-
-// const Layout = () => {
-
-//   useEffect(() => {
-//     if (Platform.OS === 'android') {
-//       SplashScreen.hide();
-//     }
-//   }, []);
-
-  
-
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator
-//         initialRouteName="Login"
-//         screenOptions={{
-//           headerShown: false,
-//           gestureEnabled: true,
-//           gestureDirection: 'horizontal',
-//         }}>
-//         <Stack.Screen name="Login" component={Login} options={options} />
-//         <Stack.Screen
-//           name="ForgotPassword"
-//           component={ForgotPassword}
-//           options={options}
-//         />
-//         <Stack.Screen name="SignUp" component={SignUp} options={options} />
-//         <Stack.Screen
-//           name="TextToSpeech"
-//           component={TextToSpeech}
-//           options={options}
-//         />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// };
-
-// export default Layout;
-
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -60,7 +11,7 @@ const Layout = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="SplashScreen" // Set SplashScreen as the first screen
+        initialRouteName="SplashScreen" 
         screenOptions={{
           headerShown: false,
           gestureEnabled: true,
